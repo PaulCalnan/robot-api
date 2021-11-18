637abd227b669383f87bee2eab8ba0ab931f07dd
Paul Calnan <pcalnan71@gmail.com>
2021-11-19 05:36:00 +1100

    changed card image order

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
   return (
     <div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img alt='bills' src={`https://www.fillmurray.com/${id + 200}/200`} />
+      <img alt='bills' src={`https://www.fillmurray.com/${id + 211}/200`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
